1342f49f2fdfee5307309605c58dd4773bf0fb9d
camerarts <152046303+camerarts@users.noreply.github.com>
2025-12-01 14:01:42 +0800

    Update StoryboardImages.tsx
    2

--- pages/StoryboardImages.tsx
+++ pages/StoryboardImages.tsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { ProjectData, StoryboardFrame, PromptTemplate } from '../types';
